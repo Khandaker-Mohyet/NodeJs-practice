401fa14b1e62858afb1e1609daf987b212e68670
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-30 17:54:11 +0600

    file read and write sync

---
+++ module-13/fs.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+const text = "Learning file system"
+
+fs.writeFileSync("./hello.txt", text)
+
+const data = fs.readFileSync("./hello.txt",{encoding: "utf-8"});
+
+console.log(data);
