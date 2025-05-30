5b695d680e16af40a27cf22341e5b63307a2af00
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-30 19:09:49 +0600

    read and file system async

--- module-13/fs.js
+++ module-13/fs.js
@@ -1,9 +1,36 @@
 const fs = require("fs")
 
-const text = "Learning file system"
+// const text = "Learning file system"
 
-fs.writeFileSync("./hello.txt", text)
+// fs.writeFileSync("./hello.txt", text)
 
-const data = fs.readFileSync("./hello.txt",{encoding: "utf-8"});
+// const data = fs.readFileSync("./hello.txt",{encoding: "utf-8"});
 
-console.log(data);
+// console.log(data);
+
+console.log("Task 1")
+
+
+
+const text = 'node js';
+
+fs.writeFile('./hello.txt', text, {encoding: "utf-8"}, (err, data) => {
+  if (err) {
+    console.log('Error writing file:', err);
+    return;
+  }
+  console.log('After writing', data);
+});
+
+
+fs.readFile('./hello.txt', {encoding:"utf-8"}, (err, data)=>{
+    if(err){
+        console.log("Something went wrong",err)
+        return
+    }
+    console.log(data)
+});
+
+
+
+console.log('Task 3')
