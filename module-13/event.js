98106c6b9bfecd33c8ef79e18d6454ca7bd16e52
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-30 17:21:34 +0600

    event listerner doen

--- module-13/event.js
+++ module-13/event.js
@@ -1,4 +1,19 @@
 const EventEmitter = require('node:events');
 
-class MyEmitter extends EventEmitter {}
+class SchoolBell extends EventEmitter {}
+
+const schoolBell = new SchoolBell();
+
+schoolBell.on("ring",()=>{
+    console.log("Yahoo class says")
+})
+schoolBell.on("ring",()=>{
+    console.log("Shat have another class")
+})
+schoolBell.on("broken",()=>{
+    console.log("Oh no! class ke ar says hoba na")
+})
+
+schoolBell.emit("ring")
+schoolBell.emit("broken")
 
