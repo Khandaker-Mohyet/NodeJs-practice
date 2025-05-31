fa077342223dd7942346cff3062a8b2f595ee221
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-01 00:58:40 +0600

    update todo

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -5,73 +5,112 @@ const fs = require("fs")
 const filePath = path.join(__dirname, './db/todo.json')
 
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
 
     const url = new URL(req.url, `http://${req.headers.host}`);
     const pathname = url.pathname
     // console.log(url, "url")
 
     // get all todos
-    
-    if(pathname === "/todos" && req.method === "GET"){
-        const data = fs.readFileSync(filePath, {encoding: "utf-8"})
-        res.writeHead(200,{
-            "content-type":"application/json",
+
+    if (pathname === "/todos" && req.method === "GET") {
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" })
+        res.writeHead(200, {
+            "content-type": "application/json",
         })
         res.end(data)
 
         // Post todos
 
-    } else if(pathname === "/todos/create-todo" && req.method === "POST"){
+    }
 
-        let data =""
+    // create todo
+    else if (pathname === "/todos/create-todo" && req.method === "POST") {
 
-        req.on("data", (chunk)=>{
+        let data = ""
+
+        req.on("data", (chunk) => {
             data = data + chunk
         })
 
-        req.on("end", ()=>{
-            
-            const {title, body} = JSON.parse(data);
-            
+        req.on("end", () => {
+
+            const { title, body } = JSON.parse(data);
+
             const createdAt = new Date().toLocaleString();
 
-            const allTodos = fs.readFileSync(filePath,{encoding: "utf-8"});
+            const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
 
             const parsedAllTodos = JSON.parse(allTodos)
             console.log(allTodos)
 
-            parsedAllTodos.push({title, body, createdAt})
+            parsedAllTodos.push({ title, body, createdAt })
 
-            fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), {encoding: "utf-8"})
+            fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), { encoding: "utf-8" })
 
-            res.end(JSON.stringify({title, body, createdAt},null, 2))
+            res.end(JSON.stringify({ title, body, createdAt }, null, 2))
         })
 
-        
-    }else if(pathname === "/todo" && req.method === "GET"){
+
+    }
+
+    // get single todo
+    else if (pathname === "/todo" && req.method === "GET") {
         const title = url.searchParams.get("title");
-        console.log(title)
-        const data = fs.readFileSync(filePath, {encoding: "utf-8"})
+        // console.log(title)
+        const data = fs.readFileSync(filePath, { encoding: "utf-8" })
         const parsedData = JSON.parse(data)
 
-        const todo = parsedData.find((todo)=> todo.title === title)
+        const todo = parsedData.find((todo) => todo.title === title)
 
         const stringifiedTodo = JSON.stringify(todo)
-        
-        res.writeHead(200,{
-            "content-type":"application/json",
+
+        res.writeHead(200, {
+            "content-type": "application/json",
         })
         res.end(stringifiedTodo)
 
-        res.end("single todo")
-
     }
-    else{
+
+    // update todo
+    else if (pathname === "/todos/update-todo" && req.method === "PATCH") {
+        const title = url.searchParams.get("title");
+        let data = "";
+
+        req.on("data", (chunk) => {
+            data = data + chunk;
+        });
+
+        req.on("end", () => {
+            const { body } = JSON.parse(data);
+
+            const allTodos = fs.readFileSync(filePath, { encoding: "utf-8" });
+            const parsedAllTodos = JSON.parse(allTodos);
+
+            const todoIndex = parsedAllTodos.findIndex(
+                (todo) => todo.title === title
+            );
+
+            parsedAllTodos[todoIndex].body = body;
+
+            fs.writeFileSync(filePath, JSON.stringify(parsedAllTodos, null, 2), {
+                encoding: "utf-8",
+            });
+
+            res.end(
+                JSON.stringify(
+                    { title, body, createdAt: parsedAllTodos[todoIndex].createdAt },
+                    null,
+                    2
+                )
+            );
+        });
+    }
+    else {
         res.end("Route not found")
     }
 })
 
-server.listen(5000, "127.0.0.1", ()=>{
+server.listen(5000, "127.0.0.1", () => {
     console.log("Server listening to port 5000")
 })
