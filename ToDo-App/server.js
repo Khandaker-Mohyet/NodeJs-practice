1651b415794c29d9eb2d3daa40d620a8c37a1582
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-31 20:17:01 +0600

    get and post working

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,7 +1,18 @@
 const http = require("http")
 
 const server = http.createServer((req,res)=>{
-    res.end("Welcome to ToDo App Server")
+    // console.log(req.url, req.method)
+    // res.end("Welcome to ToDo App Server")
+    if(req.url === "/todos" && req.method === "GET"){
+        res.writeHead(200,{
+            "content-type":"text/plain"
+        })
+        res.end("Hello ToDos")
+    } else if(req.url === "/todos/create-todo" && req.method === "POST"){
+        res.end("Todo Created")
+    }else{
+        res.end("Route not found")
+    }
 })
 
 server.listen(5000, "127.0.0.1", ()=>{
