b326fd3c7e71f7ccd8b6c1f9815f01ea7a1d5608
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-31 20:35:35 +0600

    content type

--- ToDo-App/server.js
+++ ToDo-App/server.js
@@ -1,13 +1,39 @@
 const http = require("http")
 
+const data = [
+  {
+    "title": "prisma",
+    "body": "Learning prisma",
+    "createdAt": "5/18/2025, 1:25:02 AM"
+  },
+  {
+    "title": "typescript",
+    "body": "learning node",
+    "createdAt": "5/18/2025, 1:25:12 AM"
+  },
+  {
+    "title": "express",
+    "body": "Learning advanced express",
+    "createdAt": "5/29/2025, 5:55:16 AM"
+  },
+  {
+    "title": "mongodb",
+    "body": "learning mongodb",
+    "createdAt": "5/29/2025, 5:55:46 AM"
+  }
+]
+
 const server = http.createServer((req,res)=>{
     // console.log(req.url, req.method)
     // res.end("Welcome to ToDo App Server")
     if(req.url === "/todos" && req.method === "GET"){
         res.writeHead(200,{
-            "content-type":"text/plain"
+            "content-type":"application/json",
         })
-        res.end("Hello ToDos")
+        // res.setHeader("content-type", "text/plain");
+        // res.setHeader("email", "ph2@gmail.com");
+        // res.statusCode = 201;
+        res.end(JSON.stringify(data))
     } else if(req.url === "/todos/create-todo" && req.method === "POST"){
         res.end("Todo Created")
     }else{
