8b52e114c2095f80edcdf47c0d66fc240a66e1ca
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-01 21:49:04 +0600

    nodemon install

--- module-14/dist/app.js
+++ module-14/dist/app.js
@@ -6,6 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
+    res.send('Khandaker Mohyet work station and I am learning next level');
+});
+app.get('/todos', (req, res) => {
+    res.send('Khandaker Mohyet work station');
+});
+app.get('/todos/create-todo', (req, res) => {
     res.send('Khandaker Mohyet work station');
 });
 exports.default = app;
--- module-14/src/app.ts
+++ module-14/src/app.ts
@@ -3,6 +3,12 @@ const app:Application = express()
 
 
 app.get('/', (req: Request, res: Response) => {
+  res.send('Khandaker Mohyet work station and I am learning next level')
+})
+app.get('/todos', (req: Request, res: Response) => {
+  res.send('Khandaker Mohyet work station')
+})
+app.get('/todos/create-todo', (req: Request, res: Response) => {
   res.send('Khandaker Mohyet work station')
 })
 
