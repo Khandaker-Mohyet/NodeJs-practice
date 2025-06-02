f0635286dedafcd11244e49a5ee4de2214c8ed8d
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-02 13:11:01 +0600

    connect on mongodb

--- module-14/src/server.ts
+++ module-14/src/server.ts
@@ -1,9 +1,28 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
 import app from "./app";
 const port = 5000
 
 let server;
 
+
+const uri = "mongodb+srv://mongodb:nrmXqzUhIs0qRMdq@cluster0.47f5u.mongodb.net/todosDB?retryWrites=true&w=majority&appName=Cluster0";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
 const bootstrap = async () =>{
+  await client.connect();
+  console.log("connected to Mongodb")
+  
+
+
     server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
